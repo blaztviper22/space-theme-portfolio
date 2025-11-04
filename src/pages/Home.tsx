edcf0e90df2533b9005fe75a05aa376a5b44aeff
Berthem <blaztviper22@gmail.com>
2025-11-04 15:18:30 +0800

    fixing small bugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar"
 import ProjectsSection from "../components/ProjectsSection"
 import SkillsSection from "../components/SkillsSection"
 import StarBackground from "../components/StarBackground"
-import ThemeToggle from "../components/ThemeToggle"
 import ContactSection from "../components/ContactSection"
 import Footer from "../components/Footer"
 
